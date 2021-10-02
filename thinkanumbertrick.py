0e0a0a0670e2542642be8f309031b383d6e4b4b4
Vashu <github@vashu.in>
2021-10-02 19:04:05 +0530

    Code for a trick

---
+++ thinkanumbertrick.py
@@ -0,0 +1,15 @@
+#Tell the value from a number you thought
+
+print("Think of a number between 1 to 100")
+input("Press Enter to continue.")
+print("Multiply the number by 2")
+input("Press Enter to continue.")
+print("Add 10 to the product obtained")
+input("Press Enter to continue.")
+print("Divide the obtained sum by 2")
+input("Press Enter to continue.")
+print("Subtract the obtained number by the number you thought")
+input("Press Enter to continue.")
+print("The value is 5")
+
+
