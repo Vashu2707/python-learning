19ccec18f06a389d5850d95e79da869344c54bf8
Vashu Gupta <github@vashu.in>
2023-10-14 21:39:35 +0530

    learning-3

---
+++ hacktober/learning-3.py
@@ -0,0 +1,6 @@
+#perimeter of triangle
+a = float(input("Enter the length of first side : "))
+b = float(input("Enter the length of second side : "))
+c = float(input("Enter the length of third side : "))
+p = (a+b+c)
+print("Perimeter of triagle : " , p)
